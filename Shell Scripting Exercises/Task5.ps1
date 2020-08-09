81505da4737671caddea7a93e6299a3575c7205f
Largo47 <ryu46@o2.pl>
2020-08-09 11:23:02 +0200

    updating task5

--- Shell Scripting Exercises/Task5.ps1
+++ Shell Scripting Exercises/Task5.ps1
@@ -3,8 +3,5 @@ $array = @("man", "bear", "pig", "dog", "cat");
 for($i = 0; $i -lt ($array).length; $i++){
     $array[$i]
 };
-#ForEach-Object ($element in $array) {
-#    $element
-#};
 $array | ForEach-Object -Begin {Write-Output "start"} -Process {echo "$_"} -End{Write-Output "end"} ; 
 
