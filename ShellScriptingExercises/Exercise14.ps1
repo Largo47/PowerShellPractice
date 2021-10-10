fbddc9fe81ede1f04bda133f010c67265389a5d1
Largo47 <ryu46@o2.pl>
2021-10-10 20:30:19 +0200

    fixing a terrible string construction in 14 by replacing it with a method

--- ShellScriptingExercises/Exercise14.ps1
+++ ShellScriptingExercises/Exercise14.ps1
@@ -1,3 +1,3 @@
-$Date = "$("{0:D4}" -f ((Get-Date).Year))-$("{0:D2}" -f ((Get-Date).Month))-$("{0:D2}" -f ((Get-Date).Day))" 
+$Date = (Get-Date).ToShortDateString()
 $array = Get-ChildItem .\*.jpg
 foreach($item in $array){Move-Item -Path $item.Fullname -Destination ".\$($Date)-$((Get-Item $item).BaseName).jpg"  }
